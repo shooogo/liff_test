0ad484b709bde1f0a47af055628a09fd3b2f55aa
shooogo <78833941+shooogo@users.noreply.github.com>
2021-10-22 15:44:46 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655603344-pk06n8P3";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1656559522-mYeBj8Ng";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
